7776ba03180a49b4b0b39d04855518c6a2e42ee3
tobimonges <tobiasmonges@pm.me>
2024-12-19 09:32:30 -0300

    añadiendo base del proyecto

---
+++ estacionamiento/src/test/java/tm/estacionamiento/EstacionamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package tm.estacionamiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
