b8b60f072e04c4939d64cdc6e97e849df10ce0bf
tobimonges <tobiasmonges@pm.me>
2024-12-19 15:34:59 -0300

    agregado espacio estacionamiento

--- estacionamiento/src/main/java/tm/estacionamiento/entity/EspacioEstacionamientoEntity.java
+++ estacionamiento/src/main/java/tm/estacionamiento/entity/EspacioEstacionamientoEntity.java
@@ -1,4 +1,28 @@
 package tm.estacionamiento.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "espacio_estacionamiento")
 public class EspacioEstacionamientoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private long id;
+
+    @Column(nullable = false)
+    private String sector;
+
+    @Column(nullable = false)
+    private boolean ocupado;
+
+    private String matriculaVehiculo;
 }
