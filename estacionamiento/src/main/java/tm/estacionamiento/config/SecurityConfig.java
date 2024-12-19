a804fed695077734ec6284d63ba37b5197f477b9
tobimonges <tobiasmonges@pm.me>
2024-12-19 10:30:57 -0300

    agregado encriptacion de contraseña

---
+++ estacionamiento/src/main/java/tm/estacionamiento/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package tm.estacionamiento.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- estacionamiento/src/main/java/tm/estacionamiento/service/UsuarioService.java
+++ estacionamiento/src/main/java/tm/estacionamiento/service/UsuarioService.java
@@ -1,5 +1,6 @@
 package tm.estacionamiento.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import tm.estacionamiento.entity.UsuarioEntity;
 import tm.estacionamiento.repository.UsuarioRepository;
@@ -7,15 +8,19 @@ import tm.estacionamiento.repository.UsuarioRepository;
 @Service
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UsuarioService(UsuarioRepository usuarioRepository) {
+    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
         this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UsuarioEntity registrarUsuario(UsuarioEntity usuario){
         if (usuarioRepository.existsByEmail(usuario.getEmail())) {
             throw new IllegalArgumentException("El correo ya esta registrado");
         }
+        // Encripta el password
+        usuario.setContrasena(passwordEncoder.encode(usuario.getContrasena()));
         return usuarioRepository.save(usuario);
     }
 }
